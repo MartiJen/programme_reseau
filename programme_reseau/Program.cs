a4626f178ed9b4e27a72d6304704901d0fa2f159
[Martijen] <jennifer_martin@outlook.fr>
2021-07-20 00:20:17 +0200

    Ajoutez des fichiers projet.

---
+++ programme_reseau/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+
+namespace programme_reseau
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://codeavecjonathan.com/res/papillon.jpg";
+
+            var webClient = new WebClient();
+
+            Console.WriteLine("Accès au réseau...");
+            try
+            {
+                //string reponse = webClient.DownloadString(url);
+                webClient.DownloadFile(url, "papillon.jpg");
+                Console.WriteLine("Téléchargement terminé");
+            }
+            catch(WebException ex)
+            {
+                if (ex.Response != null)
+                {
+                    var statusCode = ((HttpWebResponse)ex.Response).StatusCode;
+                    if (statusCode == HttpStatusCode.NotFound)
+                    {
+                        Console.WriteLine("ERREUR RESEAU : Non trouvé");
+                    }
+                    else
+                    {
+                        Console.WriteLine("ERREUR RESEAU : " + ex.Message);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("ERREUR RESEAU : " + ex.Message);
+                }
+                
+            }
+
+        }
+    }
+}
